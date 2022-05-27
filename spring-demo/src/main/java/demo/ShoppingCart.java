f5621122f3ead39fa6a956813121a767afa1a57c
Aron <lankipkoecharo@email.com>
2022-05-27 03:43:22 +0300

    Spring-boot Aspect basics

---
+++ spring-demo/src/main/java/demo/ShoppingCart.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+    public void checkout(String status){
+//        Logging
+//        Athentication & Athorization
+//        Sanitize the data
+        System.out.println("Checking out");
+    }
+}
